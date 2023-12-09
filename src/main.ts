41acb285e24616bccddf49aca0be579d06b73c51
Ssuamje <side2850@naver.com>
2023-12-09 18:01:36 +0900

    cors 추가

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ["http://gleefy.net", "https://gleefy.net"],
+    credentials: true,
+    exposedHeaders: ['Authorization'],
+  });
   await app.listen(3000);
 }
 bootstrap();
