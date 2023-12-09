8832319809e6971a56c8b9fee43134368d9b6f0b
Ssuamje <side2850@naver.com>
2023-12-09 17:40:21 +0900

    update

---
+++ src/app.controller.ts
@@ -0,0 +1,20 @@
+// src/app.controller.ts
+
+import { Controller, Get, HttpCode } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Get('health')
+  @HttpCode(200)
+  heath() {
+    return;
+  }
+}
